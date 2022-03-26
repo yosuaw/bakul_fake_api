5be3aca6fa4ad9dc91cdfd2c868ddfa21bb55c0b
Yosua Wijaya <80568597+yosuaw@users.noreply.github.com>
2022-03-26 18:44:46 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# bakul_fake_api
+
+access rest api: https://bakul-fake-api.herokuapp.com
---
+++ db.js
@@ -0,0 +1,19 @@
+const user = require("./data/user");
+const place = require("./data/place");
+const menu = require("./data/menu");
+const cart = require("./data/cart");
+const review = require("./data/review");
+const history = require("./data/history");
+const history_menu = require("./data/history_menu");
+const faq = require("./data/faq");
+
+module.exports = {
+    user,
+    place,
+    menu,
+    cart,
+    review,
+    history,
+    history_menu,
+    faq
+}
